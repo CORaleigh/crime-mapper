d5693599cdbc289c14900a46b37cd2277ad618bf
justingreco <justin.greco@gmail.com>
2026-05-27 10:44:36 -0400

    removed api server

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/crime-mapper/",
-  server: {
-    proxy: {
-      "/crime-mapper/api": {
-        target: "http://localhost:5050",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/crime-mapper\/api/, '/api'),
-      },
-    },
-  },
+  base: "/crime-mapper/",// /crime/ for maps.raleighnc.gov, /crime-mapper/ for GitHub
+  // server: {
+  //   proxy: {
+  //     "/crime-mapper/api": {
+  //       target: "http://localhost:5050",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/crime-mapper\/api/, '/api'),
+  //     },
+  //   },
+  // },
 });
